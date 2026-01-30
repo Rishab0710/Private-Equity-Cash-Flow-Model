c779727ed51c02caf447569e4486d00fffde36b1
Rishab Lunavat <rishablunavat@gmail.com>
2026-01-30 03:55:59 +0000

    fix this ..getting cut ...fix the size

--- src/components/app/dashboard/date-picker.tsx
+++ src/components/app/dashboard/date-picker.tsx
@@ -33,7 +33,7 @@ export function DatePicker({ date, setDate }: Props) {
           {date ? format(date, 'PPP') : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent className="p-0">
         <Calendar
           mode="single"
           selected={date}
